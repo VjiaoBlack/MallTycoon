bb454de9f1947fba6612f2be2c60b45fc1690a1c
WFT <will.a.ft@gmail.com>
2015-05-22 21:55:18 -0500

    working websockets

--- server/project.clj
+++ server/project.clj
@@ -4,9 +4,10 @@
   :min-lein-version "2.0.0"
   :dependencies [[org.clojure/clojure "1.6.0"]
                  [compojure "1.3.1"]
-                 [ring/ring-defaults "0.1.2"]]
-  :plugins [[lein-ring "0.8.13"]]
-  :ring {:handler server.handler/app}
+                 [aleph "0.4.0"]
+                 [ring/ring-defaults "0.1.2"]
+                 [org.clojure/core.async "0.1.346.0-17112a-alpha"]]
+  :main server.handler
   :profiles
   {:dev {:dependencies [[javax.servlet/servlet-api "2.5"]
                         [ring-mock "0.1.5"]]}})
--- server/src/server/handler.clj
+++ server/src/server/handler.clj
@@ -1,11 +1,44 @@
 (ns server.handler
   (:require [compojure.core :refer :all]
             [compojure.route :as route]
-            [ring.middleware.defaults :refer [wrap-defaults site-defaults]]))
+            [aleph.http :as http]
+            [ring.middleware.params :as params]
+            [aleph.http :as http]
+            [byte-streams :as bs]
+            [manifold.stream :as s]
+            [manifold.deferred :as d]
+            [manifold.bus :as bus]
+            [clojure.core.async :as a]))
+
+(def non-websocket-request
+  {:status 400
+   :headers {"content-type" "application/text"}
+   :body "Erp. I need a websocket request."})
+
+(defn echo-handler
+  "Websocket echos."
+  [req]
+  (-> (http/websocket-connection req)
+      (d/chain
+       (fn [socket]
+         (s/connect socket socket)))
+      (d/catch
+          (fn [_]
+            non-websocket-request))))
 
 (defroutes app-routes
-  (GET "/" [] "Hello World")
+  (GET "/echo" [] echo-handler)
   (route/not-found "Not Found"))
 
 (def app
-  (wrap-defaults app-routes site-defaults))
+  app-routes)
+
+(defn start
+  "Start our server in the specified port"
+  [port]
+  (http/start-server app {:port port})
+  (println "Running on port" port))
+
+(defn -main [& args]
+  (let [port (or (first args) 8080)]
+    (start port)))
