1a77f9b7469fae2dc2ffe8adb78018f78afac422
Victor Jiao <VjiaoBlack@gmail.com>
2015-06-23 21:58:18 -0400

    commited basic text edit

---
+++ _site/client/tycoon.h
@@ -0,0 +1,19 @@
+#ifndef TYCOON_H
+#define TYCOON_H
+
+#include <stdio.h>
+#include <sys/time.h>
+#include "graphics/graphics.h"
+#include "graphics/input.h"
+#include "game/map.h"
+#include "game/menu.h"
+
+
+#define FPS 60
+
+
+void update();
+
+float mspf;
+
+#endif
