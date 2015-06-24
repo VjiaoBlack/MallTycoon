571fba2c29653688200e39a5523a68c7a6f168b1
Victor Jiao <VjiaoBlack@gmail.com>
2015-06-24 10:30:20 -0400

    last commit before restyling to Mall Tycoon

---
+++ _site/client/graphics/graphics.cpp
@@ -0,0 +1,190 @@
+#include "graphics.h"
+
+ViewMode view_mode(400,100);
+
+SDL_Window* window;
+SDL_Texture* texture;
+SDL_Renderer* renderer;
+
+std::map<std::string, SDL_Texture*> textures;
+
+int ViewMode::set_x_offset(int x) {
+    int old = x_offset;
+    x_offset = x;
+    return old;
+}
+int ViewMode::set_y_offset(int y) {
+    int old = y_offset;
+    y_offset = y;
+    return old;
+}
+float ViewMode::set_x_offvel(float x) {
+    float old = x_offvel;
+    x_offvel = x;
+    return old;
+}
+float ViewMode::set_y_offvel(float y) {
+    float old = y_offvel;
+    y_offvel = y;
+    return old;
+}
+float ViewMode::set_x_offacc(float x) {
+    float old = x_offacc;
+    x_offacc = x;
+    return old;
+}
+float ViewMode::set_y_offacc(float y) {
+    float old = y_offacc;
+    y_offacc = y;
+    return old;
+}
+int ViewMode::set_construction_mode(int c) {
+    int old = construction_mode;
+    construction_mode = c;
+    return old;
+}
+
+void ViewMode::update() {
+    y_offacc = 0;
+    x_offacc = 0;
+
+    float vel = 0.4;
+
+    if (keys_held[SDLK_DOWN]) {
+        y_offacc = -vel;
+    } else if (y_offvel < 0) {
+        y_offacc = vel;
+    }
+
+    if (keys_held[SDLK_UP]) {
+        y_offacc = vel;
+    } else if (y_offvel > 0) {
+        y_offacc = -vel;
+    }
+
+
+    if (!keys_held[SDLK_DOWN] && !keys_held[SDLK_UP]) {
+        if (y_offvel < vel && y_offvel > -vel) {
+            y_offvel = 0;
+            y_offacc = 0;
+        }
+    }
+
+    if (keys_held[SDLK_RIGHT]) {
+        x_offacc = -vel;
+    } else if (x_offvel < 0) {
+        x_offacc = vel;
+    }
+
+    if (keys_held[SDLK_LEFT]) {
+        x_offacc = vel;
+    } else if (x_offvel > 0) {
+        x_offacc = -vel;
+    }
+
+
+    if (!keys_held[SDLK_LEFT] && !keys_held[SDLK_RIGHT]) {
+        if (x_offvel < vel && x_offvel > -vel) {
+            x_offvel = 0;
+            x_offacc = 0;
+        }
+    }
+
+    if (y_offvel > 3) {
+        y_offacc = 0;
+        y_offvel = 3;
+    }
+
+    if (y_offvel < -3) {
+        y_offacc = 0;
+        y_offvel = -3;
+
+    }
+
+    if (x_offvel > 3) {
+        x_offacc = 0;
+        x_offvel = 3;
+
+    }
+
+    if (x_offvel < -3) {
+        x_offacc = 0;
+        x_offvel = -3;
+
+    }
+
+    x_offvel = x_offvel + x_offacc;
+    y_offvel = y_offvel + y_offacc;
+
+    x_offset = x_offset + x_offvel;
+    y_offset = y_offset + y_offvel;
+}
+
+
+void draw_line(int x1, int y1, int x2, int y2, int r, int g, int b, int a) {
+    SDL_SetRenderDrawColor( renderer, r, g, b, a );
+    SDL_RenderDrawLine(renderer, x1, y1, x2, y2);
+}
+
+void draw_rect(int x1, int y1, int x2, int y2, int r, int g, int b, int a) {
+    SDL_SetRenderDrawColor( renderer, r, g, b, a );
+
+    int ymin = (y1 < y2) ? y1 : y2;
+    int ymax = (y1 > y2) ? y1 : y2;
+
+    for (int y = ymin; y <= ymax; ++y) {
+        SDL_RenderDrawLine(renderer, x1, y, x2, y);
+    }
+}
+
+
+
+void clear_textures() {
+    textures.clear();
+}
+
+void load_textures() {
+    SDL_Texture* text;
+
+    std::string files[] = {"grass","road","water","shadow"};
+    int num_files = 4;
+
+    for (int i = 0; i < num_files; i++) {
+        /* Open the image files */
+        std::string location = "res/" + files[i] + ".png";
+        text = IMG_LoadTexture(renderer, location.c_str());
+        if (!text) {
+            fprintf(stderr, "Couldn't load %s: %s\n", location.c_str(), SDL_GetError());
+        }
+        SDL_SetRenderTarget(renderer, text);
+        textures.insert ( std::pair<std::string,SDL_Texture*>(files[i],text) );
+    }
+}
+
+void init_graphics() {
+    load_textures();
+}
+
+void init_sdl() {
+
+    if (SDL_Init(SDL_INIT_VIDEO) < 0) {
+        printf("Could not initialize SDL: %s\n", SDL_GetError());
+        exit(1);
+    }
+
+    window = SDL_CreateWindow("My Game Window",
+                        SDL_WINDOWPOS_UNDEFINED,
+                        SDL_WINDOWPOS_UNDEFINED,
+                        WINDOW_WIDTH, WINDOW_HEIGHT,
+                        SDL_WINDOW_SHOWN);
+
+    if (window == NULL) {
+        printf("Couldn't set window mode %d x %d: %s\n", 800, 640, SDL_GetError());
+        SDL_Quit();
+        exit(1);
+    }
+
+    renderer = SDL_CreateRenderer( window, 0, SDL_RENDERER_ACCELERATED);
+
+    init_graphics();
+}
---
+++ _site/client/graphics/graphics.h
@@ -0,0 +1,77 @@
+#ifndef GRAPHICS_H
+#define GRAPHICS_H
+
+#define WINDOW_WIDTH 800
+#define WINDOW_HEIGHT 640
+
+#include "SDL.h"
+#include "input.h"
+#include <math.h>
+// #include "/Library/Frameworks/SDL2_image.framework/Headers/SDL_image.h"
+#include <SDL_image.h>
+#include <iostream>
+#include <fstream>
+#include <map>
+#include <string>
+#include <vector>
+
+void draw_line(int,int,int,int, int,int,int,int);
+void draw_rect(int,int,int,int, int,int,int,int);
+
+void init_sdl();
+
+void load_textures();
+
+void clear_textures();
+
+void init_graphics();
+
+class ViewMode {
+    int x_offset, y_offset;
+    float x_offvel, y_offvel;
+    float x_offacc, y_offacc;
+
+    int construction_mode; // lets the user know what's the current construction mode.
+
+public:
+    ViewMode(){
+        x_offset = y_offset = x_offvel = y_offvel = x_offacc = y_offacc = 0;
+        construction_mode = -1;
+    }
+    ViewMode(int x, int y){x_offset = x; y_offset = y; y_offvel = x_offvel = x_offacc = y_offacc = 0;}
+    int get_x_offset(){return x_offset;}
+    int get_y_offset(){return y_offset;}
+    int set_x_offset(int x);
+    int set_y_offset(int y);
+
+    float get_x_offvel(){return x_offvel;}
+    float get_y_offvel(){return y_offvel;}
+    float set_x_offvel(float x);
+    float set_y_offvel(float y);
+
+    float get_x_offacc(){return x_offacc;}
+    float get_y_offacc(){return y_offacc;}
+    float set_x_offacc(float x);
+    float set_y_offacc(float y);
+
+    int get_construction_mode() {return construction_mode;};
+    int set_construction_mode(int c);
+
+    void update();
+};
+
+extern ViewMode view_mode;
+
+extern SDL_Window* window;
+extern SDL_Texture* texture;
+extern SDL_Renderer* renderer;
+
+extern SDL_Texture *grass;
+
+extern std::map<std::string, SDL_Texture*> textures;
+
+
+
+// TODO: this graphics world view thing.
+
+#endif
